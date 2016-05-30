ccb43fd5e8ebf3eca3dac3eddc971413dadc43a4
Vincent Tang <Vince@MacBook-Pro.local>
2016-05-30 14:06:58 -0400

    Added blog meta data (linked and styled)

--- wp-content/themes/mytheme/special-template.php
+++ wp-content/themes/mytheme/special-template.php
@@ -7,7 +7,8 @@ get_header();
 
 if (have_posts()) :
 
-while (have_posts()) : the_post(); ?>
+while (have_posts()) : the_post();
+?>
 		<article class="post page">
 			<h2><?php the_title(); ?></h2>	
 			
